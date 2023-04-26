cd5225cdc9fe75045781b67ea3e0ce9e44e7b1b1
Xaver Kainz <xaver.kainz@tum.de>
2023-04-26 20:06:43 +0200

    Add MPO class

---
+++ MPO.py
@@ -0,0 +1,10 @@
+'''MPO.py'''
+
+from SuperMPS import SuperMPS
+class MPO(SuperMPS):
+    def __init__(self, *args, xi, cutoff=1e-8):
+        super().__init__(*args, xi=xi, cutoff=cutoff)
+        if self.indices_per_node != 2:
+            raise ValueError("MPO must have 2 indices per node")
+    
+    

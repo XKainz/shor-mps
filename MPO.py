1af53b6d0c1a2976d158ad038f5f95971e47e18c
Xaver Kainz <xaver.kainz@tum.de>
2023-05-05 10:58:53 +0200

    Demand for MPO to have 2 indices per node

--- MPO.py
+++ MPO.py
@@ -3,7 +3,7 @@
 from SuperMPS import SuperMPS
 class MPO(SuperMPS):
     def __init__(self, *args, xi, cutoff=1e-8):
-        super().__init__(*args, xi=xi, cutoff=cutoff)
+        super().__init__(*args,2, xi=xi, cutoff=cutoff)
         if self.indices_per_node != 2:
             raise ValueError("MPO must have 2 indices per node")
     

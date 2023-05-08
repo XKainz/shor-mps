9da09f0a86f5acad307e3c2a8ccf1510f1e1f9f9
Xaver Kainz <xaver.kainz@tum.de>
2023-05-08 18:01:47 +0200

    Add pickable function

--- classical_shors_algorithm.py
+++ classical_shors_algorithm.py
@@ -1,6 +1,8 @@
 '''classical_shors_alorithm'''
 from qm_shors_algorithm import *
 from shor_helpers import *
+from to_pickle import *
+import pickle
 
 
 def success_prob_measurement_patch(patch,x,N):
@@ -12,22 +14,54 @@ def success_prob_measurement_patch(patch,x,N):
                 prob_success += patch[i]
     return prob_success
 
-N = 77
-x = 5
-print(x,"x")
-xi_start = 2**8
+def pickle_object(N,x,xi_start):
+    mps_fat, len_a = get_shor_mps_fat_ten(x,N,xi_start)
+    max_bond_dim_fat = mps_fat.maximum_bond_dim()
+    print(max_bond_dim_fat,"max_bond_dim_fat")
+    p_suc = []
+    ran = range(max_bond_dim_fat,2,-3)
+    for i in ran:
+        print(i,"current_xi")
+        mps, len_a = get_shor_mps_fat_ten(x,N,i)
+        patch = mps.measure_subspace(0,len_a)
+        p_suc.append(success_prob_measurement_patch(patch,x,N))
+    
+    p_suc_fat = np.zeros((len(p_suc),2))
+    p_suc_fat[:,0] = [*ran]
+    p_suc_fat[:,1] = p_suc
+    print(p_suc_fat,"p_suc_fat")
 
-p_suc = []
-ran = range(xi_start,30,-10)
-MPS, len_a  = get_shor_mps_mpo(x,N,xi_start)
-for i in ran:
-    MPS.set_xi(i)
-    print(MPS.maximum_bond_dim(),"Maximum Bond Dimension")
-    patch = MPS.measure_subspace(0,len_a)
-    p_suc.append(success_prob_measurement_patch(patch,x,N))
+    
+    mps_mpo, len_a, mpo_mpo = get_shor_mps_mpo(x,N,xi_start)
+    max_bond_dim_mps_mpo = mps_mpo.maximum_bond_dim()
+    print(max_bond_dim_mps_mpo,"max_bond_dim_mps_mpo")
+    max_bond_dim_mpo_mpo = mpo_mpo.maximum_bond_dim()
+    print(max_bond_dim_mpo_mpo,"max_bond_dim_mpo_mpo")
+    p_suc = []
+    ran = range(max(max_bond_dim_mpo_mpo,max_bond_dim_mpo_mpo),4,-3)
+    for i in ran:
+        print(i,"current_xi")
+        mps, len_a, mpo = get_shor_mps_mpo(x,N,i)
+        patch = mps.measure_subspace(0,len_a)
+        p_suc.append(success_prob_measurement_patch(patch,x,N))
+    p_suc_mpo = np.zeros((len(p_suc),2))
+    p_suc_mpo[:,0] = [*ran]
+    p_suc_mpo[:,1] = p_suc
+    print(p_suc_mpo,"p_suc_mpo")
 
-#print(p_suc)
-plt.figure(figsize=(15,7))
-plt.plot([*ran],p_suc)
-plt.show()
+    to_pickle = ToPickle(N,
+                         x,
+                         mps_mpo,
+                         mps_fat,
+                         p_suc_mpo,
+                         p_suc_fat,
+                         mpo_mpo,
+                         max_bond_dim_mpo_mpo,
+                         max_bond_dim_mps_mpo,
+                         max_bond_dim_fat)
+    
+    pickle.dump(to_pickle,open("to_pickle"+str(to_pickle.N)+"_"+str(to_pickle.x)+".pkl","wb"))
+
+pickle_object(13*5,7,2**8)
+
 
--- qm_shors_algorithm.py
+++ qm_shors_algorithm.py
@@ -1,7 +1,8 @@
-import MPS as mps
+from MPS import *
 import Gates as gates
 import numpy as np
-import MPO as mpo
+from MPO import *
+from MPSU import *
 import matplotlib.pyplot as plt
 import shor_helpers as sh
 import Circuits as circ
@@ -17,24 +18,55 @@ def get_shor_mps_mpo(x,N,xi):
     len_total = len_a+len_b
 
     # Create the MPS
-    MPS = mps.MPS.create_MPS_init_to_N(1,len_total,xi=xi,cutoff=cutoff)
+    mps = MPS.create_MPS_init_to_N(1,len_total,xi=xi,cutoff=cutoff)
 
     #apply hadamards gates to all sites of len_a
     for i in range(len_a):
-        MPS.apply_1_site_gate(gates.H,i)
+        mps.apply_1_site_gate(gates.H,i)
+
+    #get control U gates
+    cu_gates = gates.cx_pow_2k_mod_N(N,x,len_a)
+
+    mpo = 0
+
+    #apply the controlled U gates
+    for i in range(len_a):
+        mpo = MPO.create_MPO_from_tensor(cu_gates[len_a-1-i],xi,cutoff=cutoff)
+        mps.apply_mpo(mpo,len_a-1)
+        for j in range(len_a-2,i-1,-1):
+            mps.apply_2_site_gate(gates.SWAP,j) 
+
+    first_mpo = mpo
+
+    #apply the inverse fourier transform
+    mps = circ.fourier_transform_MPS(mps,0,len_a,inv=True)
+    t1 = time.time()
+    print("MPS created in",t1-t0,"seconds")
+    return mps, len_a, first_mpo
+
+def get_shor_mps_fat_ten(x,N,xi):
+    t0 = time.time()
+    len_b = int(np.ceil(np.log2(N)))
+    len_a = 2*len_b+3
+    print("len_a",len_a,"len_b",len_b)
+
+    # Create the MPSU
+    mpsu = MPSU.create_MPSU_init_to_1(len_a,len_b,xi,cutoff)
+    #apply hadamards gates to all sites of len_a
+    for i in range(len_a):
+        mpsu.apply_1_site_gate(gates.H,i)
 
     #get control U gates
     cu_gates = gates.cx_pow_2k_mod_N(N,x,len_a)
 
     #apply the controlled U gates
     for i in range(len_a):
-        mpocu = mpo.MPO(cu_gates[len_a-1-i],xi=xi,cutoff=cutoff)
-        MPS.apply_mpo(mpocu,len_a-1)
+        mpsu.apply_fat_xgate(cu_gates[len_a-1-i])
         for j in range(len_a-2,i-1,-1):
-            MPS.apply_2_site_gate(gates.SWAP,j) 
+            mpsu.apply_2_site_gate(gates.SWAP,j) 
 
     #apply the inverse fourier transform
-    MPS = circ.fourier_transform_MPS(MPS,0,len_a,inv=True)
+    mpsu = circ.fourier_transform_MPS(mpsu,0,len_a,inv=True)
     t1 = time.time()
     print("MPS created in",t1-t0,"seconds")
-    return MPS, len_a
+    return mpsu, len_a
